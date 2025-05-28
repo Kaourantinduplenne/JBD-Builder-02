d0db8536b930eb91d0bb751d8e086e51da29280c
Kaourantinduplenne <kaourantin.duplenne@gmail.com>
2025-05-28 13:19:31 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -4,8 +4,7 @@ const RigJBDBuilder = dynamic(() => import('../components/RigJBDBuilder'), { ssr
 export default function Home() {
   return (
     <main className="p-4 max-w-5xl mx-auto">
-      <h1 className="text-2xl font-bold mb-4">Rig Job By Design</h1>
-      <RigJBDBuilder />
+            <RigJBDBuilder />
     </main>
   );
 }
